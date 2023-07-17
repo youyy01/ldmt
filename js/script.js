af527aac14149e5463dca529920a44564afdd836
youyy01 <137478067+youyy01@users.noreply.github.com>
2023-07-17 16:30:39 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,55 +3,118 @@ $(document).ready(function(){
 	
 	
 	// gnb
-	/*$("#gnb>ul>li.m2>a").click(function(){
-		if(!$(this).parent("li.m2").hasClass("active")){
+	$("#gnb>ul>li.m1>a").click(function(e){
+		e.preventDefault();
+		if(!$(this).parent("li").hasClass("active")){
+			$("#gnb>ul>li.m1").removeClass("active");
+			$("#gnb>ul>li.m1 .monitor-list");
+			$(this).parent("li").addClass("active");
 			$("#gnb>ul>li.m2").removeClass("active");
-			$("#gnb>ul>li.m2 .monitor-list");
-			$(this).parent("li.m2").addClass("active");
-			$(this).parent("li.m2").find(".monitor-list");
+			$("#gnb>ul>li.m3").removeClass("active");
+			$("#gnb>ul>li.m4").removeClass("active");
+			$("html").addClass("cross-opened");
+			$("html").removeClass("monitor-opened");
+			$("html").removeClass("statis-opened");
+			$("html").removeClass("admin-opened");
 		}else{
-			$(this).parent("li.m2").removeClass("active");
-			$(this).parent("li.m2").find(".monitor-list");
+			$(this).parent("li").removeClass("active");
+			$("html").removeClass("cross-opened");
 		}	
 	});
 
 	$("#gnb>ul>li.m2>a").click(function(e){
 		e.preventDefault();
-		if($("html").hasClass("monitor-opened")){
+		if(!$(this).parent("li").hasClass("active")){
 			$("#gnb>ul>li.m2").removeClass("active");
-			$("html").removeClass("monitor-opened");
-			setMyCookie();
-			resizeStage();
-		}else{			
+			$(this).parent("li").addClass("active");
+			$("#gnb>ul>li.m1").removeClass("active");
+			$("#gnb>ul>li.m3").removeClass("active");
+			$("#gnb>ul>li.m4").removeClass("active");
 			$("html").addClass("monitor-opened"); 
-			$("#gnb>ul>li.m2").addClass("active");
-			setMyCookie();
-			resizeStage();
-		}
-	});*/
-
-
-
-	function setMyCookie() {		
-		myCookieVal = $('html').hasClass('monitor-opened') ? 'isActive' : 'notActive';
-		$.cookie('gnbToggle', myCookieVal);       
-	}
-
-	if ($.cookie('gnbToggle') == 'isActive') {
-		$('html').addClass('monitor-opened');  
-		$("#gnb>ul>li.m2").addClass("active");
-	} else {
-		$('html').removeClass('monitor-opened');
-		$("#gnb>ul>li.m2").removeClass("active");
-
-	}
-
-	$("#gnb>ul>li.m2>a").on('click', function(e) {
-		$("html").toggleClass("monitor-opened");
-		$("#gnb>ul>li.m2").toggleClass("active");
-		setMyCookie();
-		resizeStage();
+			$("html").removeClass("cross-opened");
+			$("html").removeClass("statis-opened");	
+			$("html").removeClass("admin-opened");
+		}else{
+			$(this).parent("li").removeClass("active");
+			$("html").removeClass("monitor-opened");
+		}	
 	});
+
+	$("#gnb>ul>li.m3>a").click(function(e){
+		e.preventDefault();
+		if(!$(this).parent("li").hasClass("active")){
+			$("#gnb>ul>li.m3").removeClass("active");
+			$(this).parent("li").addClass("active");
+			$("#gnb>ul>li.m1").removeClass("active");
+			$("#gnb>ul>li.m2").removeClass("active");
+			$("#gnb>ul>li.m4").removeClass("active");
+			$("html").addClass("statis-opened"); 
+			$("html").removeClass("monitor-opened"); 
+			$("html").removeClass("cross-opened");	
+			$("html").removeClass("admin-opened");
+		}else{
+			$(this).parent("li").removeClass("active");
+			$("html").removeClass("statis-opened");
+		}	
+	});
+
+	$("#gnb>ul>li.m4>a").click(function(e){
+		e.preventDefault();
+		if(!$(this).parent("li").hasClass("active")){
+			$("#gnb>ul>li.m4").removeClass("active");
+			$(this).parent("li").addClass("active");
+			$("#gnb>ul>li.m1").removeClass("active");
+			$("#gnb>ul>li.m2").removeClass("active");
+			$("#gnb>ul>li.m3").removeClass("active");
+			$("html").addClass("admin-opened"); 
+			$("html").removeClass("statis-opened"); 
+			$("html").removeClass("monitor-opened"); 
+			$("html").removeClass("cross-opened");			
+		}else{
+			$(this).parent("li").removeClass("active");
+			$("html").removeClass("admin-opened");
+		}	
+	});
+
+	
+
+	$(".sitelogo>a").click(function(e){
+		e.preventDefault();
+		$("#gnb>ul>li").removeClass("active");
+		$("html").removeClass("monitor-opened");
+		$("html").removeClass("statis-opened");
+		$("html").removeClass("cross-opened");
+		$("html").removeClass("admin-opened");
+	});
+
+	
+	// 진입차량 통계 
+	$("#map .links > li > a").click(function(e){
+		e.preventDefault();
+		t = $(this).parent('li');
+		if (t.hasClass('active')) {
+			t.removeClass('active');
+			t.find('.link-content').slideUp('fast');
+			return false; 
+		}else {
+			$("#map .links li").removeClass('active');
+			t.addClass('active');	
+			if(t.find('div').hasClass('link-content')){		
+				$("#map .links li .link-content").slideUp('fast');	
+				t.find('.link-content').slideDown('fast');	
+				return false;
+			}
+		}
+	});
+
+	$("#map .links .close").click(function(e){
+		e.preventDefault();
+		$(".link-content").slideUp('fast');
+		$("#map .links li").removeClass('active');
+	});
+
+
+	
 	
 
 	// select2
